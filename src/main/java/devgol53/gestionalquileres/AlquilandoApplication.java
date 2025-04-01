abe71b353f8b9458c1e233451af942709346d3b9
emilianoross11 <emilianoross649@gmail.com>
2025-04-01 16:07:16 -0300

    first commit

---
+++ src/main/java/devgol53/gestionalquileres/AlquilandoApplication.java
@@ -0,0 +1,14 @@
+package devgol53.gestionalquileres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilandoApplication.class, args);
+		System.out.println("Hola");
+	}
+
+}
---
+++ src/test/java/devgol53/gestionalquileres/AlquilandoApplicationTests.java
@@ -0,0 +1,13 @@
+package devgol53.gestionalquileres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
